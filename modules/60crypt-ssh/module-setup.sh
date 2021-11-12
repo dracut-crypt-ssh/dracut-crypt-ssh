656da0af685ea4f3328ad085e125927f7cddf88c
Zach Dykstra <dykstra.zachary@gmail.com>
2021-11-11 20:35:57 -0600

    Consider it a success if at least one key is generated and converted

--- modules/60crypt-ssh/module-setup.sh
+++ modules/60crypt-ssh/module-setup.sh
@@ -19,15 +19,21 @@ install() {
   [[ -z "${dropbear_port}" ]] && dropbear_port=222
   [[ -z "${dropbear_acl}" ]] && dropbear_acl=/root/.ssh/authorized_keys
   local tmpDir=$(mktemp -d --tmpdir dracut-crypt-ssh.XXXX)
+  trap '[ -n ${tmpDir} ] && [ -d ${tmpDir} ] && rm -rf ${tmpDir}' EXIT INT TERM
   local genConf="${tmpDir}/crypt-ssh.conf"
   local installConf="/etc/crypt-ssh.conf"
+  local default_keytypes="rsa ecdsa ed25519"
+  local relaxFailures convertSuccess
 
-  # Make sure dropbear_keytypes has a value and everything is lowercase
   if [[ -z "${dropbear_keytypes}" ]]; then
-    dropbear_keytypes="rsa ecdsa ed25519"
-  else
-    dropbear_keytypes="$(echo "${dropbear_keytypes}" | tr '[:upper:]' '[:lower:]')"
+    dropbear_keytypes="${default_keytypes}"
+  elif [[ "${dropbear_keytypes}" == "any" ]]; then
+    dropbear_keytypes="${default_keytypes}"
+    convertSuccess=0
+    relaxFailures=1
   fi
+
+  dropbear_keytypes="${dropbear_keytypes,,}"
 
   #start writing the conf for initramfs include
   echo -e "#!/bin/bash\n\n" > $genConf
@@ -36,43 +42,62 @@ install() {
 
   #go over different encryption key types
   for keyType in $dropbear_keytypes; do
-    keyType=$(echo "$keyType" | tr '[:upper:]' '[:lower:]')
     eval state=\$dropbear_${keyType}_key
-    local msgKeyType=$(echo "$keyType" | tr '[:lower:]' '[:upper:]')
+    local msgKeyType="${keyType^^}"
 
     [[ -z "$state" ]] && state=GENERATE
 
     local osshKey="${tmpDir}/${keyType}.ossh"
     local dropbearKey="${tmpDir}/${keyType}.dropbear"
     local installKey="/etc/dropbear/dropbear_${keyType}_host_key"
+
+    local output
     
     case ${state} in
       GENERATE )
-        ssh-keygen -t $keyType -f $osshKey -q -N "" -m PEM || {
-          derror "SSH ${msgKeyType} key creation failed"
-          rm -rf "$tmpDir"
-          return 1
-        }
+        if ! output="$( ssh-keygen -t $keyType -f $osshKey -q -N "" -m PEM 2>&1 )" ; then
+          if [ -z "${relaxFailures}" ]; then
+            derror "SSH ${msgKeyType} key creation failed"
+            derror "${output}"
+            return 1
+          else
+            dwarn "SSH ${msgKeyType} key creation failed"
+            dwarn "${output}"
+            continue
+          fi
+        fi
         
         ;;
       SYSTEM )
         local sysKey=/etc/ssh/ssh_host_${keyType}_key
-        [[ -f ${sysKey} ]] || {
-          derror "Cannot locate a system SSH ${msgKeyType} host key in ${sysKey}"
-          derror "Start OpenSSH for the first time or use ssh-keygen to generate one"
-          return 1
-        }
+        if ! [[ -f ${sysKey} ]]; then
+          if [ -z "${relaxFailures}" ]; then
+            derror "Cannot locate a system SSH ${msgKeyType} host key in ${sysKey}"
+            derror "Start OpenSSH for the first time or use ssh-keygen to generate one"
+            return 1
+          else
+            dwarn "Cannot locate a system SSH ${msgKeyType} host key in ${sysKey}"
+            dwarn "Start OpenSSH for the first time or use ssh-keygen to generate one"
+            continue
+          fi
+        fi
 
         cp $sysKey $osshKey
         cp ${sysKey}.pub ${osshKey}.pub
         
         ;;
       * )
-        [[ -f ${state} ]] || {
-          derror "Cannot locate a system SSH ${msgKeyType} host key in ${state}"
-          derror "Please use ssh-keygen to generate this key"
-          return 1
-        }
+        if ! [[ -f ${state} ]]; then
+          if [ -z "${relaxFailures}" ]; then
+            derror "Cannot locate a system SSH ${msgKeyType} host key in ${state}"
+            derror "Please use ssh-keygen to generate this key"
+            return 1
+          else
+            dwarn "Cannot locate a system SSH ${msgKeyType} host key in ${state}"
+            dwarn "Please use ssh-keygen to generate this key"
+            continue
+          fi
+        fi
         
         cp $state $osshKey
         cp ${state}.pub ${osshKey}.pub
@@ -80,11 +105,19 @@ install() {
     esac
     
     #convert the keys from openssh to dropbear format
-    dropbearconvert openssh dropbear $osshKey $dropbearKey > /dev/null 2>&1 || {
-      derror "dropbearconvert for ${msgKeyType} key failed"
-      rm -rf "$tmpDir"
-      return 1
-    }
+    if ! output="$( dropbearconvert openssh dropbear $osshKey $dropbearKey 2>&1 )"; then
+      if [ -z "${relaxFailures}" ]; then
+        derror "dropbearconvert for ${msgKeyType} key failed"
+        derror "${output}"
+        return 1
+      else
+        dwarn "dropbearconvert for ${msgKeyType} key failed"
+        dwarn "${output}"
+        continue
+      fi
+    else
+      convertSuccess="$(( convertSuccess + 1 ))"
+    fi
 
     #install and show some information
     local keyFingerprint=$(ssh-keygen -l -f "${osshKey}")
@@ -98,6 +131,12 @@ install() {
     echo "dropbear_${keyType}_bubble='$keyBubble'" >> $genConf
 
   done
+
+  if [[ -n "${relaxFailures}" ]] && [[ "${convertSuccess}" -eq 0 ]]; then
+    derror "At least one SSH key must be generated and converted correctly"
+    rm -rf "$tmpDir"
+    return 1
+  fi
 
   inst_rules "$moddir/50-udev-pty.rules"
 

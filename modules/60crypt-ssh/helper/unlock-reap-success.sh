d3e77cf72a9a1652e7628da5c8264c09bea83af0
Piraty <mail@piraty.dev>
2023-05-02 02:23:12 +0200

    fix unlock-reap-success.sh for posix shells
    remove bashism
    
    relying on /bin/sh to know about bourne-syntax is pretty risky

--- modules/60crypt-ssh/helper/unlock-reap-success.sh
+++ modules/60crypt-ssh/helper/unlock-reap-success.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 SYSTEMD_CRYPTSETUP="$(ps -C systemd-cryptsetup -o pid=)"
-if [[ $? -eq 0 ]]; then
- # Systemd method
- kill -9 ${SYSTEMD_CRYPTSETUP}
+if [ "$?" -eq 0 ]; then
+	# Systemd method
+	kill -9 "${SYSTEMD_CRYPTSETUP}"
 else
- # Older method
- pkill cryptroot-ask
+	# Older method
+	pkill cryptroot-ask
 fi
-

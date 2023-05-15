6f7c7db54c0b1fa67b1eb44bbf131af719cd103a
uriesk <uriesk@posteo.de>
2023-05-15 19:48:41 +0200

    disable TIOCSTI again if we were the ones who enabled it

--- modules/60crypt-ssh/dropbear-stop.sh
+++ modules/60crypt-ssh/dropbear-stop.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -r /tmp/legacy_tiocsti.default ] && [ -w /proc/sys/dev/tty/legacy_tiocsti ]; then
+  cat /tmp/legacy_tiocsti.default > /proc/sys/dev/tty/legacy_tiocsti
+fi
+
 [ -f /tmp/dropbear.pid ] || exit 0
 read main_pid < /tmp/dropbear.pid
 kill -STOP ${main_pid} 2>/dev/null

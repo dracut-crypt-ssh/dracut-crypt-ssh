26c1a7709fd34c911274c53b8392a249963de50e
justin <justin.brown@fandingo.org>
2023-09-22 14:08:58 -0500

    Busybox Compat
    Busybox's `ps` command does not support the '-C' argument. That only
    works with procps-ng version.
    
    Simplify the script to use `pkill`. Both the procps-ng and busybox
    versions work properly.

--- modules/60crypt-ssh/helper/unlock-reap-success.sh
+++ modules/60crypt-ssh/helper/unlock-reap-success.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-SYSTEMD_CRYPTSETUP="$(ps -C systemd-cryptsetup -o pid=)"
-if [ "$?" -eq 0 ]; then
-	# Systemd method
-	kill -9 "${SYSTEMD_CRYPTSETUP}"
-else
-	# Older method
-	pkill cryptroot-ask
-fi
+pkill systemd-cryptsetup || pkill cryptroot-ask
+

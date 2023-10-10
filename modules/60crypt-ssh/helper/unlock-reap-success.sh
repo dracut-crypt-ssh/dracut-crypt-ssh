de3c2b9ffceae77f04b3aa2c05cf0f377f0c39a8
Mattias Cockburn <mattias.cockburn@posteo.net>
2023-10-10 12:17:22 +0200

    fix(unlock-reap-success): naive workaround for Archlinux issues
    `pkill` from procps-ng 4.0.4 needs `-f` to search for the full
    commandline

--- modules/60crypt-ssh/helper/unlock-reap-success.sh
+++ modules/60crypt-ssh/helper/unlock-reap-success.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 pkill systemd-cryptsetup || pkill cryptroot-ask
+# Some versions of `pkill` need -f:
+# pkill: pattern that searches for process name longer than 15 characters will result in zero matches
+# Try `pkill -f' option to match against the complete command line.
+pkill -f systemd-cryptsetup || pkill -f cryptroot-ask
 
